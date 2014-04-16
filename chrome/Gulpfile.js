a482693dc0e858397d6fa93e31c9a33800437739
johann <git@johann-hofmann.com>
2014-04-16 16:57:28 +0100

    restructuring to support multiple browsers

---
+++ chrome/Gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+
+var jade = require('gulp-jade');
+
+var emojis = require('../emojis.json');
+
+gulp.task('popup', function() {
+  gulp.src('../shared/popup.jade')
+    .pipe(jade({
+      locals: {
+        emojis: emojis
+      }
+    }))
+    .pipe(gulp.dest("./build/"));
+});
+
+gulp.task('manifest', function() {
+  gulp.src("./manifest.json").pipe(gulp.dest("./build/"));
+});
+
+gulp.task('shared', function() {
+  gulp.src([
+    "../shared/**/*",
+    "!../shared/popup.jade"
+  ]).pipe(gulp.dest("./build/"));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  gulp.watch('../shared/popup.jade', ['build']);
+  gulp.watch('../shared/src/*.js', ['build']);
+  gulp.watch('../shared/style/*.css', ['build']);
+});
+
+gulp.task('build', ['popup', 'shared', 'manifest']);
+
+gulp.task('default', ['build', 'watch']);
