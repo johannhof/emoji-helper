57aa36751d113a7172287935579233fe7a453290
johann <git@johann-hofmann.com>
2014-05-15 12:06:05 +0100

    change bookmarklet brackets

--- bookmarklet/bookmarklet.js
+++ bookmarklet/bookmarklet.js
@@ -1,38 +1,38 @@
 var toggleEmojiBookmarklet = function () {
   //var SERVER_URL = "http://localhost:8000";
-  var SERVER_URL = "http://johannhof.github.io/emoji-helper/bookmarklet/";
+  var SERVER_URL = 'http://johannhof.github.io/emoji-helper/bookmarklet/';
 
-  var container = document.createElement("div");
-  var frame = document.createElement("iframe");
+  var container = document.createElement('div');
+  var frame = document.createElement('iframe');
   frame.src = SERVER_URL;
 
-  frame.style.height = "350px";
-  frame.style.width = "510px";
-  frame.style.border = "1px solid lightgray";
+  frame.style.height = '350px';
+  frame.style.width = '510px';
+  frame.style.border = '1px solid lightgray';
 
-  container.style.height = "350px";
-  container.style.width = "510px";
-  container.style.boxShadow = "2px 2px 10px black";
-  container.style.position = "fixed";
-  container.style.left = "calc(50% - 255px)";
-  container.style.top = "10%";
+  container.style.height = '350px';
+  container.style.width = '510px';
+  container.style.boxShadow = '2px 2px 10px black';
+  container.style.position = 'fixed';
+  container.style.left = 'calc(50% - 255px)';
+  container.style.top = '10%';
 
 
-  var closeButton = document.createElement("button");
-  closeButton.style.position = "absolute";
-  closeButton.style.height = "30px";
-  closeButton.style.width = "30px";
-  closeButton.style.right = "-15px";
-  closeButton.style.top = "-15px";
-  closeButton.style.border = "5px solid white";
-  closeButton.style.borderRadius = "20px";
-  closeButton.style.backgroundColor = "white";
-  closeButton.style.boxShadow = "0px 0px 1px black";
-  closeButton.style.backgroundImage = "url(" + SERVER_URL + "/img/emoji/heavy_multiplication_x.png)";
-  closeButton.style.backgroundRepeat = "no-repeat";
-  closeButton.style.backgroundPosition = "center";
-  closeButton.style.backgroundSize = "contain";
-  closeButton.style.cursor = "pointer";
+  var closeButton = document.createElement('button');
+  closeButton.style.position = 'absolute';
+  closeButton.style.height = '30px';
+  closeButton.style.width = '30px';
+  closeButton.style.right = '-15px';
+  closeButton.style.top = '-15px';
+  closeButton.style.border = '5px solid white';
+  closeButton.style.borderRadius = '20px';
+  closeButton.style.backgroundColor = 'white';
+  closeButton.style.boxShadow = '0px 0px 1px black';
+  closeButton.style.backgroundImage = 'url(' + SERVER_URL + '/img/emoji/heavy_multiplication_x.png)';
+  closeButton.style.backgroundRepeat = 'no-repeat';
+  closeButton.style.backgroundPosition = 'center';
+  closeButton.style.backgroundSize = 'contain';
+  closeButton.style.cursor = 'pointer';
 
   closeButton.onclick = function () {
     container.parentNode.removeChild(container);
