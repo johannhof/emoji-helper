a89c9cb7c6c4718be7ed01d6a1869444ac32061b
Johann <git@johann-hofmann.com>
2014-12-22 15:58:25 +0100

    Start to move common parts out of gulpfiles

---
+++ gulp-common.js
@@ -0,0 +1,11 @@
+exports.emoji = [
+      '../shared/img/emoji/clock9.png',
+      '../shared/img/emoji/smiley.png',
+      '../shared/img/emoji/cherry_blossom.png',
+      '../shared/img/emoji/bell.png',
+      '../shared/img/emoji/oncoming_automobile.png',
+      '../shared/img/emoji/symbols.png',
+      '../shared/img/emoji/hatched_chick.png',
+      '../shared/img/emoji/snake.png',
+      '../shared/img/emoji/heart.png'
+];
