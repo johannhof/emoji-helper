ef3669b15f654eb43d04cfed09073d48e28f6caa
Johann <git@johann-hofmann.com>
2015-06-18 23:20:30 +0200

    Bump to v1.1

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-V=1.0.1
+V=1.1.0
 
 release: build
 	rm -rf release/latest
--- shared/src/popup.js
+++ shared/src/popup.js
@@ -3,7 +3,7 @@
   // local vars for linting (and performance)
   var vendor = window.vendor;
 
-  var VERSION = "1.0.1";
+  var VERSION = "1.1.0";
 
   // upper bar
   var logos = document.querySelectorAll(".group-logo");
