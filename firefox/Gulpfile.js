3fdfa2082b321814fdc92ece5404257f86fa9639
Jack Union <verteletskydmitry@gmail.com>
2016-02-05 05:50:37 +0300

    cfx to jpm firefox package,json

--- firefox/Gulpfile.js
+++ firefox/Gulpfile.js
@@ -8,6 +8,25 @@ var emojis = require("../shared/data/sprite.json");
 
 var build = "./build/";
 
+var Transform = require('stream').Transform;
+function transformPackageJSON() {
+  var transformStream = new Transform({ objectMode: true });
+  transformStream._transform = function(file, encoding, done) {
+    var parsed = JSON.parse(file.contents.toString());
+    file.contents = new Buffer(
+      JSON.stringify(Object.assign({}, parsed,
+        {
+          'id': parsed.id + '@jetpack',
+          'main': 'lib/main.js'
+        }
+      ), null, 2)
+    );
+    this.push(file)
+    done();
+  }
+  return transformStream;
+}
+
 gulp.task("popup", function() {
   gulp.src("../shared/popup.jade")
     .pipe(jade({
@@ -37,7 +56,9 @@ gulp.task("shared", function() {
     "!../shared/popup.jade",
     "!../shared/img/emoji/*"
   ]).pipe(gulp.dest(build + "data/"));
-  gulp.src("../package.json").pipe(gulp.dest(build));
+  gulp.src("../package.json")
+    .pipe(transformPackageJSON())
+    .pipe(gulp.dest(build));
   gulp.src("../shared/icons/icon48.png").pipe(rename("icon.png")).pipe(gulp.dest(build + "data/"));
   gulp.src("../shared/icons/icon_alt.png").pipe(rename("icon64.png")).pipe(gulp.dest(build + "data/"));
 });
